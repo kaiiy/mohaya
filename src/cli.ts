c226ea62056cc747bf54cb0e750c3ddd0af9b204
k <81578246+kaiiy@users.noreply.github.com>
2025-05-17 19:59:15 +0900

    chore: update packages

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,8 @@
 import OpenAI from "jsr:@openai/openai@^4.95.1";
-import { z } from "https://deno.land/x/zod@v3.24.2/mod.ts";
+import { z } from "https://deno.land/x/zod@v3.24.4/mod.ts";
 import { parseArgs } from "https://deno.land/std@0.224.0/cli/parse_args.ts";
 
-const VERSION = "1.9.1";
+const VERSION = "1.9.2";
 
 const flags = parseArgs(Deno.args, {
   alias: {
